b1d29aaffbcc79845c62a5015e52cda760ba002d
hashim janjua <hashimj@hotmail.co.uk>
2022-09-06 16:50:30 +0100

    Added PG search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
+  # def index
+  #   @books = Book.all
+  # end
+
   def index
-    @books = Book.all
+    if params[:query].present?
+      @books = Book.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @books = Book.all
+    end
   end
 end
