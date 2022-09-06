d4e888ac9b4353737e5e5c2f0a2c84a2fdced261
Claudio Paz <claudio.paz.gonzalez@gmail.com>
2022-09-06 17:54:18 +0200

    footer fixed to bottom of the page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :books, only: :index
+  resources :books, only: [:index, :show]
 end
