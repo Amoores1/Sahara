53a619c902ba814d33115d95cb8ab7482114401b
ernesto baquero <ernest.baquero@gmail.com>
2022-09-07 13:09:45 +0100

    adding map_controller; map.html; initializer/geocoder

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -1,2 +1,12 @@
 class BookstoresController < ApplicationController
+  def index
+    @bookstores = Bookstore.all
+    # The `geocoded` scope filters only bookstores with coordinates
+    @markers = @bookstores.geocoded.map do |bookstore|
+      {
+        lat: bookstore.latitude,
+        lng: bookstore.longitude
+      }
+    end
+  end
 end
--- app/models/bookstore.rb
+++ app/models/bookstore.rb
@@ -1,3 +1,6 @@
 class Bookstore < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
