caccdb423a83409bb71f4f3facd04ca2cbdbf5c7
ernesto baquero <ernest.baquero@gmail.com>
2022-09-07 13:31:07 +0100

    add info_windows_map to use it into map_controller

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -6,6 +6,8 @@ class BookstoresController < ApplicationController
       {
         lat: bookstore.latitude,
         lng: bookstore.longitude
+        # take more info to the view
+        # info_window_map: render_to_string(partial: "info_window_map", locals: {bokstore: bokstore})
       }
     end
   end
