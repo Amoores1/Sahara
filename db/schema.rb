4d82d2db0edb019c65b5d6afab0b128effd8ab62
ernesto baquero <ernest.baquero@gmail.com>
2022-09-07 15:20:19 +0100

    adding new seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_101034) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_154932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,8 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_101034) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_bookstores_on_user_id"
   end
 
