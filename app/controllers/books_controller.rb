a7e71ca89543dbd6a7088d768dd2354c4751e2d8
Aaron Moores <mooresaaron@gmail.com>
2022-09-08 19:22:03 +0100

    filter changes complete, for now...

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,13 @@
 class BooksController < ApplicationController
 
   def index
-    if params[:query].present?
-      @books = Book.where("title ILIKE ?", "%#{params[:query]}%")
-    elsif params[:genre].present?
-      @books = Book.where(genre: params[:genre].capitalize)
-    elsif params[:condition].present?
-      @books = Book.where(condition: params[:condition].capitalize)
-    elsif params[:from].present? && params[:to].present?
-      @books = Book.where(:year => params[:from]..params[:to])
-    else
-      @books = Book.all
-    end
+    @books = Book.all
+    # search
+    @books = @books.where("title ILIKE ? OR genre ILIKE ? OR author ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%") if params[:query].present?
+    # filters
+    @books = @books.where(genre: params[:genre].capitalize) if params[:genre].present?
+    @books = @books.where(condition: params[:condition]) if params[:condition].present?
+    @books = @books.where(:year => params[:from]..params[:to]) if params[:from].present? && params[:to].present?
   end
 
   def show
