c561528c7c062a0a051a481b3d451666b3266131
hashim janjua <hashimj@hotmail.co.uk>
2022-09-09 14:13:26 +0100

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_09_124741) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,12 +113,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_124741) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "basket_items", "carts"
-
   add_foreign_key "basket_items", "orders"
   add_foreign_key "basket_items", "physical_books"
   add_foreign_key "bookstores", "users"
