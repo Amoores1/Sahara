d7396eb3229387636126a5b3eaaf3b5e1f6dcda0
ernesto baquero <ernest.baquero@gmail.com>
2022-09-15 11:06:52 +0100

    404 working after change to false consider_all_requests_local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports. change to false to test errors_controller.
+  # Show full error reports. Change to false to test errors_controller.
   config.consider_all_requests_local = true
 
   # Enable server timing
